d183ae5f526e71efa2066f3aed814ecf2f674c7d
rizkitriandani <rizkitriandani@yahoo.co.id>
2022-07-13 20:54:27 +0700

    Done fetching data fron API and put it into model.

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,47 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {
-  //TODO: Implement HomeController
+import '../../../data/models/blog_post.dart';
+import '../../../services/http_service.dart';
+import '../../../utils/logger.dart';
+import 'package:dio/dio.dart' as d;
 
-  final count = 0.obs;
+class HomeController extends GetxController {
+  HttpService httpService = HttpService();
+  RxBool isLoading = false.obs;
+
+  List<BlogPost> blogPostList = [];
+ 
+
   @override
-  void onInit() {
+  void onInit() async {
+    logger.d("INIT HOME CONTROLLER");
+    print("INIT HOME CONTROLLER");
+    await httpService.init();
+    await fetchBlogPosts();
+
     super.onInit();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  Future<void> fetchBlogPosts() async {
+    dynamic result;
+    isLoading(true);
+    try {
+      logger.d("Fetching blog posts");
+      result = await httpService.request(url: "/posts", method: Method.GET);
+      result.data
+          .forEach((element) => blogPostList.add(BlogPost.fromJson(element)));
+    } catch (err) {
+      logger.d(err);
+      throw err;
+    } finally {
+      isLoading(false);
+    }
   }
 
   @override
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
