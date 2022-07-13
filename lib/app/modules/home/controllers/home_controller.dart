e13f05257ecd31c6e58a815ee55b2a99bd539099
rizkitriandani <rizkitriandani@yahoo.co.id>
2022-07-13 22:54:59 +0700

    added search functionality but still not accurate.

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,23 @@
 import 'dart:convert';
 
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 import '../../../data/models/blog_post.dart';
 import '../../../services/http_service.dart';
 import '../../../utils/logger.dart';
-import 'package:dio/dio.dart' as d;
 
 class HomeController extends GetxController {
   HttpService httpService = HttpService();
   RxBool isLoading = false.obs;
 
+  List<BlogPost> allBlogPostList = [];
   List<BlogPost> blogPostList = [];
- 
+
+  TextEditingController searchController = TextEditingController();
 
   @override
   void onInit() async {
-    logger.d("INIT HOME CONTROLLER");
-    print("INIT HOME CONTROLLER");
     await httpService.init();
     await fetchBlogPosts();
 
@@ -30,14 +30,29 @@ class HomeController extends GetxController {
     try {
       logger.d("Fetching blog posts");
       result = await httpService.request(url: "/posts", method: Method.GET);
-      result.data
-          .forEach((element) => blogPostList.add(BlogPost.fromJson(element)));
+      result.data.forEach(
+          (element) => allBlogPostList.add(BlogPost.fromJson(element)));
+      blogPostList = allBlogPostList;
     } catch (err) {
       logger.d(err);
       throw err;
     } finally {
       isLoading(false);
     }
+    update();
+  }
+
+  void searchPost(String query) {
+    final suggestions = allBlogPostList.where((post) {
+      final blogTitle = post.title!.toLowerCase();
+      final input = query.toLowerCase();
+
+
+      return blogTitle.contains(input);
+    }).toList();
+
+    blogPostList = suggestions;
+    update();
   }
 
   @override
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../../utils/logger.dart';
 import '../../../widgets/loading_view.dart';
@@ -13,19 +14,57 @@ class HomeView extends GetView<HomeController> {
     logger.d("BUILD");
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
+        title: Text("Blog Post"),
       ),
-      body: Obx(() => controller.isLoading.value
-          ? LoadingView()
-          : ListView.separated(
-              itemCount: controller.blogPostList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Text(controller.blogPostList[index].title.toString());
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                  Divider(height: 3, color: Colors.red),
-            )),
+      body: GetBuilder<HomeController>(
+          init: controller,
+          initState: (_) {},
+          builder: (_) => controller.isLoading.value
+              ? LoadingView()
+              : Column(
+                  children: [
+                    TextField(
+                      controller: controller.searchController,
+                      style: GoogleFonts.openSans(
+                          color: Colors.black, fontSize: 15),
+                      decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText: 'Search a blog post',
+                          hintStyle: GoogleFonts.openSans(
+                              color: const Color(0xff6F7075)),
+                          prefixIcon: const Icon(
+                            Icons.search,
+                            color: Color(0xff6F7075),
+                          )),
+                        onChanged:(String) => controller.searchPost(controller.searchController.text),
+                    ),
+                    Expanded(
+                      child: ListView.separated(
+                        itemCount: controller.blogPostList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final post = controller.allBlogPostList[index];
+
+                          return InkWell(
+                            onTap: () {},
+                            child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 5),
+                                child: Text(post.title.toString())),
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) =>
+                            Divider(height: 3, color: Colors.grey),
+                      ),
+                    ),
+                  ],
+                )),
     );
   }
+
+  void search() {}
 }
